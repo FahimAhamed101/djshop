bce99eeffe9db594c2317994290c47561f02bfea
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2023-05-22 10:45:35 +0600

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'f^=5pd1lso=1zxm*da!r$=@%o937%zv+4n7pvtgkoiuk3tkps('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 #
-ALLOWED_HOSTS = ['Domain']
+ALLOWED_HOSTS = [config('Domain')]
 
 
 # Application definition
